8b9cae58ee4058c1057d304dfd92327308c71ccd
Oliver S <r3zn1k@bluewin.ch>
2017-12-23 14:26:09 +0100

    actually include the configuration

--- read.php
+++ read.php
@@ -8,6 +8,7 @@ header("Content-Type: application/json; charset=UTF-8");
 $instance = isset($_GET['instance']) ? $_GET['instance'] : die('Missing parameter: instance');
 $date = isset($_GET['date']) ? $_GET['date'] : die('Missing parameter: date');
 
+include_once 'config/configuration.php';
 include_once 'inc/Database.php';
 include_once 'inc/Message.php';
 
