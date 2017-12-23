28a1a3b120547a5e714dbaf27fe277d910164c41
Oliver S <r3zn1k@bluewin.ch>
2017-12-23 15:01:07 +0100

    fix the sql statement that fetches the message entries

--- inc/Message.php
+++ inc/Message.php
@@ -9,13 +9,13 @@ class Message {
     }
 
     public function readEntries($instance, $date) {
-        $query = "SELECT message_entry.pk AS pk, message_entry.entry AS AS entry, message_entry_type.type AS type, message_entry_type.sorting AS sorting" .
-            "FROM message_entry" .
-            "LEFT JOIN message_entry_type ON (message_entry_type.pk=message_entry.fk_message_entry_type)" .
-            "LEFT JOIN message ON (message.pk=message_entry.fk_message)" .
-            "LEFT JOIN instance ON (instance.pk=message.fk_instance)" .
-            "WHERE instance.name = ':instance_name' AND message.date = ':message_date'" .
-            "ORDER BY message_entry.sorting";
+        $query = "SELECT message_entry.pk AS pk, message_entry.entry AS entry, message_entry_type.type AS type, message_entry.sorting AS sorting" .
+            " FROM message_entry" .
+            " LEFT JOIN message_entry_type ON (message_entry_type.pk=message_entry.fk_message_entry_type)" .
+            " LEFT JOIN message ON (message.pk=message_entry.fk_message)" .
+            " LEFT JOIN instance ON (instance.pk=message.fk_instance)" .
+            " WHERE instance.name = :instance_name AND message.date = :message_date" .
+            " ORDER BY message_entry.sorting";
 
         $statement = $this->database->getConnection()->prepare($query);
 
